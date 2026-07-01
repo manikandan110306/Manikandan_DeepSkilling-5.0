9ebf225e417d83cfe03e1d2d2a0cd16e9efc3496
Manikandan T <manit110306@gmail.com>
2026-07-01 15:58:11 +0530

    module 6

---
+++ Module-6/demo/src/main/java/com/example/demo/service/CountryService1.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.Country1;
+import com.example.demo.repository.CountryRepository1;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CountryService1 {
+
+    @Autowired
+    private CountryRepository1 countryRepository;
+
+    @Transactional
+    public List<Country1> getAllCountries() {
+
+        return countryRepository.findAll();
+
+    }
+
+}
